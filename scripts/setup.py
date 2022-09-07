d65beede7103154a0c979969e97b112e97e44ca6
Felipe Nunes <fnap@cin.ufpe.br>
2022-09-07 13:51:23 -0300

    refactor script

--- scripts/setup.py
+++ scripts/setup.py
@@ -37,12 +37,8 @@ def run(command):
 def check_dependencies():
     raise NotImplementedError
 
-
-def install_sequence():
-    check_is_sudo()
-
-    # Flight stack
-    cprint('\x1B[34m')('installing PX4-Autopilot')
+def install_px4_autopilot():
+    cprint('\x1B[34m')('installing PX4-Autopilot...')
     try:
         run('git clone https://github.com/PX4/PX4-Autopilot.git --recursive')
     except:
@@ -50,15 +46,37 @@ def install_sequence():
     else:
         cprint('\x1B[32m')('[OK] PX4-Autopilot installed')
 
-    cprint('\x1B[34m')('building PX4-Autopilot')
+
+def build_px4_autopilot():
+    cprint('\x1B[34m')('building PX4-Autopilot...')
     try:
         run('bash ./PX4-Autopilot/Tools/setup/ubuntu.sh')
     except:
         cprint('\x1B[31m')('[ERROR] could not install PX4-Autopilot')
     else:
-        cprint('\x1B[32m')('[OK] PX4-Autopilot installed')
-    
-    # Communication libraries
+        cprint('\x1B[32m')('[OK] PX4-Autopilot built')
+
+
+def download_qgroundcontrol():
+    cprint('\x1B[34m')('installing QGroundControl...')
+    try:
+        run('wget https://d176tv9ibo4jno.cloudfront.net/latest/QGroundControl.AppImage')
+    except:
+        cprint('\x1B[31m')('[ERROR] could not get QGroundControl')
+    else:
+        cprint('\x1B[32m')('[OK] QGroundControl downloaded')
+
+def update_qgroundcontrol_permissions():
+    cprint('\x1B[34m')('updating QGroundControl permissions...')
+    try:
+        run('chmod +x ./QGroundControl.AppImage')
+    except:
+        cprint('\x1B[31m')('[ERROR] could not update QGroundControl permissions')
+    else:
+        cprint('\x1B[32m')('[OK] QGroundControl permissions updated')
+
+
+def install_comm_mavlink():
     cprint('\x1B[34m')('installing mavlink...')
     try:
         run('pip3 install mavlink')
@@ -67,6 +85,8 @@ def install_sequence():
     else:
         cprint('\x1B[32m')('[OK] mavlink installed')
 
+
+def install_comm_mavsdk():
     cprint('\x1B[34m')('installing mavsdk...')
     try:
         run('pip3 install mavsdk')
@@ -76,5 +96,20 @@ def install_sequence():
         cprint('\x1B[32m')('[OK] mavsdk installed')
 
 
+def install_sequence():
+    # Check permissions
+    check_is_sudo()
+
+    # Flight stack
+    install_px4_autopilot()
+    build_px4_autopilot()
+    download_qgroundcontrol()
+    update_qgroundcontrol_permissions()
+
+    # Communication libraries
+    install_comm_mavlink()
+    install_comm_mavsdk
+
+
 if __name__ == '__main__':
-    install_sequence()
+    check_is_sudo()
